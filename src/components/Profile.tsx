da0d46486bb8f944c4244d50ecdcd8c3f027a6f0
Pavel Khrysto <miki2012miki@gmail.com>
2022-12-04 12:33:59 +0300

    SW 13

---
+++ src/components/Profile.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4mYAeykNjPS5oMZ8-n6lc2my3fgr51M0_Zg&usqp=CAU"
+                    alt="porshe"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>New post</div>
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+                <div>
+                    post3
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
