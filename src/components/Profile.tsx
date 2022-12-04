56c0050fbe89641b63ca7aad006a524a21c4e9a3
Pavel Khrysto <miki2012miki@gmail.com>
2022-12-04 23:18:24 +0300

    14/css-modules

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import classes from "./Profile.module.css"
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={classes.content}>
             <div>
                 <img
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4mYAeykNjPS5oMZ8-n6lc2my3fgr51M0_Zg&usqp=CAU"
@@ -15,14 +15,14 @@ const Profile = () => {
                 My post
                 <div>New post</div>
             </div>
-            <div>
-                <div>
+            <div className="post">
+                <div className={classes.item}>
                     post1
                 </div>
-                <div>
+                <div className={classes.item}>
                     post2
                 </div>
-                <div>
+                <div className={classes.item}>
                     post3
                 </div>
             </div>
