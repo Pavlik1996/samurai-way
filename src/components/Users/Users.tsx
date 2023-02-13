5d2d1df032b65247269e6c893d94d30f86cfa5f3
Pavel Khrysto <miki2012miki@gmail.com>
2023-02-13 16:59:47 +0300

    62 - follow-unfollow api2

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import s from "./users.module.css";
 import userPhoto from "../../assets/images/user.jpg";
 import {UserPageType, UsersType} from "../../redux/store";
@@ -6,74 +6,88 @@ import {NavLink} from "react-router-dom";
 import axios from "axios";
 
 type PropsUsersType = {
-    items: UsersType[],
-    pageSize: number,
-    totalCount: number,
-    follow: (id: number) => void,
-    unFollow: (id: number) => void,
-    currentPage: number,
-    onClickHandlerChangePage: (page: number) => void
-}
+    items: UsersType[];
+    pageSize: number;
+    totalCount: number;
+    follow: (id: number) => void;
+    unFollow: (id: number) => void;
+    currentPage: number;
+    onClickHandlerChangePage: (page: number) => void;
+};
 
-type FollowType = {
-    data: {}
-    fieldsErrors: []
-    messages: []
-    resultCode: number
-}
+type PostDeleteType = {
+    resultCode: number;
+    messages: string[];
+    data: {};
+};
 
 export const Users = (props: PropsUsersType) => {
-
-    let pagesCount = Math.ceil(props.totalCount / props.pageSize)
-    let pages = []
+    let pagesCount = Math.ceil(props.totalCount / props.pageSize);
+    let pages = [];
     for (let i = 1; i <= 30; i++) {
-        pages.push(i)
+        pages.push(i);
     }
 
     const followHandler = (id: number) => {
-        axios.post<FollowType>(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, null, {withCredentials: true})
-            .then(r => {
-                if (r.data.resultCode === 0) props.follow(id)
-            })
-
-    }
+        axios
+            .post<PostDeleteType>(
+                `https://social-network.samuraijs.com/api/1.0/follow/${id}`,
+                null,
+                {withCredentials: true}
+            )
+            .then((r) => {
+                if (r.data.resultCode === 0) {
+                    props.follow(id);
+                }
+            });
+    };
     const unFollowHandler = (id: number) => {
-        axios.delete<FollowType>(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {withCredentials: true})
-            .then(r => {
-                if (r.data.resultCode === 0) props.unFollow(id)
-            })
-    }
+        axios
+            .delete<PostDeleteType>(
+                `https://social-network.samuraijs.com/api/1.0/follow/${id}`,
+                {withCredentials: true}
+            )
+            .then((r) => {
+                if (r.data.resultCode === 0) props.unFollow(id);
+            });
+    };
 
     return (
         <div>
             <div>
-                {pages.map(el => {
+                {pages.map((el) => {
                     return (
-                        <span key={el}
-                              onClick={() => props.onClickHandlerChangePage(el)}
-                              className={props.currentPage === el ? s.selected : ''}> {el} </span>
-                    )
+                        <span
+                            key={el}
+                            onClick={() => props.onClickHandlerChangePage(el)}
+                            className={props.currentPage === el ? s.selected : ""}
+                        >
+              {` ${el} `}
+            </span>
+                    );
                 })}
                 <span>{pagesCount}</span>
-
             </div>
-            {
-                props.items.map(el => <div key={el.id} style={{display: 'flex'}}>
-                    <NavLink to={'/profile/' + el.id}>
-                        <img alt={'ava'} className={s.userPhoto}
-                             src={el.photos.small !== null ? el.photos.small : userPhoto}/>
+            {props.items.map((el) => (
+                <div key={el.id} style={{display: "flex"}}>
+                    <NavLink to={"/profile/" + el.id}>
+                        <img
+                            alt={"ava"}
+                            className={s.userPhoto}
+                            src={el.photos.small !== null ? el.photos.small : userPhoto}
+                        />
                     </NavLink>
 
                     <div>
-                        {el.followed
-                            ? <button onClick={() => unFollowHandler(el.id)}>UnFollow</button>
-                            : <button onClick={() => followHandler(el.id)}>Follow</button>
-                        }
+                        {el.followed ? (
+                            <button onClick={() => unFollowHandler(el.id)}>UnFollow</button>
+                        ) : (
+                            <button onClick={() => followHandler(el.id)}>Follow</button>
+                        )}
                     </div>
                     {el.name}
-                </div>)
-            }
+                </div>
+            ))}
         </div>
     );
 };
-
