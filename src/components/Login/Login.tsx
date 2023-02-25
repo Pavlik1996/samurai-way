1dba9f12a52429bd256bdffa67025c2f616ed9bb
Pavel Khrysto <miki2012miki@gmail.com>
2023-02-26 00:33:08 +0300

    redux-form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React from "react";
+import { Field, InjectedFormProps, reduxForm } from "redux-form";
 
-export const Login = () => {
-    return (
-        <h2>
-            Login
-        </h2>
-    );
+type FormDataType = {
+  login: string;
+  password: string;
+  rememberme: boolean;
 };
 
+export const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field placeholder={"login"} name={"login"} component={"input"} />
+      </div>
+      <div>
+        <Field placeholder={"Password"} name={"password"} component={"input"} />
+      </div>
+      <div>
+        <Field component={"input"} name={"rememberme"} type={"checkbox"} />
+        <span>remember me</span>
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  );
+};
+
+const LoginReduxForm = reduxForm<FormDataType>({
+  form: "login", // уникальное строкове имя
+})(LoginForm);
+
+export const Login = () => {
+  const onSubmit = (fromData: FormDataType) => {
+    console.log(fromData);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </div>
+  );
+};
--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -4,6 +4,7 @@ import { dialogsReducer } from "./dialogs-reducer";
 import { usersReducer } from "./users-reducer";
 import { AuthReducer } from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 
 let reducers = combineReducers({
   profilePage: profileReducer,
@@ -11,6 +12,7 @@ let reducers = combineReducers({
   usersPage: usersReducer,
   // sidebar: sidebarReducer
   auth: AuthReducer,
+  form: formReducer,
 });
 
 export type AppStateType = ReturnType<typeof reducers>;
