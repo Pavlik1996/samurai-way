b64af4815760af8ffda67704f444bef8ce6c040a
Pavlik1996 <miki2012miki@gmail.com>
2023-03-30 12:47:12 +0300

    add pagination for Users.tsx

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,33 +1,60 @@
 import React from "react";
-import { UsersType } from "../../redux/users-reducer";
-import { Paginagor } from "../common/Paginator/Paginator";
-import { User } from "./User";
+import {UsersType} from "../../redux/users-reducer";
+// import { Paginagor } from "../common/Paginator/Paginator";
+import {User} from "./User";
+import {Pagination, Select} from '@mui/material'
+
 
 type PropsUsersType = {
-  items: UsersType[];
-  pageSize: number;
-  totalCount: number;
-  follow: (id: number) => void;
-  unFollow: (id: number) => void;
-  currentPage: number;
-  onClickHandlerChangePage: (page: number) => void;
-  isFollowing: number[];
+    items: UsersType[];
+    pageSize: number;
+    totalCount: number;
+    follow: (id: number) => void;
+    unFollow: (id: number) => void;
+    currentPage: number;
+    onClickHandlerChangePage: (page: number) => void;
+    isFollowing: number[];
 };
 
 export const Users: React.FC<PropsUsersType> = ({
-  items, pageSize, totalCount, follow, unFollow, currentPage, onClickHandlerChangePage, isFollowing
-}) => {
+                                                    items,
+                                                    pageSize,
+                                                    totalCount,
+                                                    follow,
+                                                    unFollow,
+                                                    currentPage,
+                                                    onClickHandlerChangePage,
+                                                    isFollowing
+                                                }) => {
 
-  return (
-    <>
-      <Paginagor onClickHandlerChangePage={onClickHandlerChangePage} currentPage={currentPage} />
-      {items.map(user => <User
-        follow={follow}
-        isFollowing={isFollowing}
-        unFollow={unFollow}
-        user={user}
-        key={user.id} />
-      )}
-    </>
-  );
+
+
+    const onChangeCallback = (event: any, page: number) => {
+        onClickHandlerChangePage(page)
+    }
+
+
+    return (
+        <>
+            {/*<Paginagor onClickHandlerChangePage={onClickHandlerChangePage} currentPage={currentPage} />*/}
+
+            <Pagination
+                page={currentPage}
+                count={ Math.ceil(totalCount / 10)}
+                onChange={onChangeCallback}
+                color="secondary"
+                boundaryCount={2}
+                style={{display: 'flex', justifyContent: 'center'}}
+            />
+
+
+            {items.map(user => <User
+                follow={follow}
+                isFollowing={isFollowing}
+                unFollow={unFollow}
+                user={user}
+                key={user.id}/>
+            )}
+        </>
+    );
 };
